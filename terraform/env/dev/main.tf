bfca0cef1eddb3bf07413829cd60b876165188b8
Sivanajani <sivanajani@gmail.com>
2026-02-24 16:14:07 +0100

    feat: Cloud test

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -26,6 +26,7 @@ module "vm" {
   project-id = var.project-id
   zone = var.zone
   vm-service-account-email = var.vm-service-account-email
+  machine-type = "e2-small"
   frontend_virtual_host     = "dev.shaperag.com"
   frontend_letsencrypt_host = "dev.shaperag.com"
   letsencrypt_email         = var.notify-email
